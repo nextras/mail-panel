b0e4396cc68c509d3312841ad74520a8be3074c4
Jan Drabek <me@jandrabek.cz>
2012-06-23 16:15:50 +0200

    Core: predelano na nove nette, bez presenteru a bez spousty sablon

--- MailPanel.php
+++ MailPanel.php
@@ -1,112 +1,131 @@
 <?php
 /**
- * Extension for Debugger bar
+ * Extension for Debugger bar which shows sent e-mails
  * 
  * @author Jan Drábek
- * @version 1.0
- * @copyright GNU-GPLv3
+ * @version 2.0
+ * @copyright New BSD
  */
 
-class MailPanel extends Object implements IBarPanel {
-	const VERSION = "1.0";
-	/** @var bool */
-	private static $registered = FALSE;
+namespace JanDrabek\Diagnostics;
+use Nette;
+
+class MailPanel extends Nette\Application\UI\Control implements Nette\Diagnostics\IBarPanel {
 	
-	/** @var Session */
-	private static $container;
+	/** @var Nette\Http\Request */
+	private static $request;
 	
-	public function __construct() {
-		// Get mail storage
-		self::$container = Environment::getSession("session-dummy");
-		// Check (and fix) how many items is displayed
-		if(empty(self::$container->count)) {
-			self::$container->count = 3;
-		} 
+	/** @var Nette\Http\Session */
+	private static $session;
+	
+	/**
+	 * Create this panel and handle request for actions (trasmitted in requests)
+	 * 
+	 * @param Nette\Http\Request $request
+	 * @param Nette\Http\Session $session 
+	 */
+	public function __construct(Nette\Http\Request $request, Nette\Http\Session $session) {
+		self::$session = $session;
+		self::$request = $request;
+		$what = $request->getQuery("mail-panel");
+		switch ($what) {
+			case 'delete':
+				$this->handleDeleteAll();
+				break;
+			case 'less':
+				$this->handleShowLess(self::DEFAULT_COUNT);
+				break;
+			case 'more':
+				$this->handleShowMore(self::DEFAULT_COUNT);
+				break;
+			default:
+				break;
+		}
+		if(is_numeric($what) && $what >= 0) {
+			$this->handleDelete($what);
+		}
 	}
+	
+	const DEFAULT_COUNT = 3;
 	
 	/**
 	 * Returns panel ID.
 	 * @return string
-	 * @see Nette\IDebugPanel::getId()
 	 */
 	public function getId() {
-		return "mail-panel";
+		return __CLASS__;
 	}
 
 	/**
 	 * Renders HTML code for custom tab.
 	 * @return string
-	 * @see Nette\IDebugPanel::getTab()
 	 */
 	public function getTab() {
 		return '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAITSURBVBgZpcHLThNhGIDh9/vn7/RApwc5VCmFWBPi1mvwAlx7BW69Afeu3bozcSE7E02ILjCRhRrds8AEbKVS2gIdSjvTmf+TYqLu+zyiqszDMCf75PnnnVwhuNcLpwsXk8Q4BYeSOsWpkqrinJI6JXVK6lSRdDq9PO+19vb37XK13Hj0YLMUTVVyWY//Cf8IVwQEGEeJN47S1YdPo4npDpNmnDh5udOh1YsZRcph39EaONpnjs65oxsqvZEyTaHdj3n2psPpKDLBcuOOGUWpZDOG+q0S7751ObuYUisJGQ98T/Ct4Fuo5IX+MGZr95jKjRKLlSxXxFxOEmaaN4us1Upsf+1yGk5ZKhp8C74H5ZwwCGO2drssLZZo1ouIcs2MJikz1oPmapHlaoFXH1oMwphyTghyQj+MefG+RblcoLlaJG/5y4zGCTMikEwTctaxXq/w9kuXdm9Cuzfh9acujXqFwE8xmuBb/hCwl1GKAnGccDwIadQCfD9DZ5Dj494QA2w2qtQW84wmMZ1eyFI1QBVQwV5GiaZOpdsPaSwH5HMZULi9UmB9pYAAouBQbMHHrgQcnQwZV/KgTu1o8PMgipONu2t5KeaNiEkxgAiICDMCCFeEK5aNauAOfoXx8KR9ZOOLk8P7j7er2WBhwWY9sdbDeIJnwBjBWBBAhGsCmiZxPD4/7Z98b/0QVWUehjkZ5vQb/Un5e/DIsVsAAAAASUVORK5CYII=" /> Sent mails';
 	}
 
 	/**
-	 * Renders HTML code for custom panel.
-	 * @return string
-	 * @see Nette\IDebugPanel::getPanel()
+	 * Show content of panel
+	 * @return type 
 	 */
 	public function getPanel() {
 		ob_start();
-		$template = new FileTemplate(__DIR__. "/MailPanel.latte");
-		$template->registerFilter(new LatteFilter);
-		$template->count = self::$container->count;
+		// Get data from session
+		$section = $this->getStorage();
+		$template = $this->createTemplate();
+		$template->setFile(__DIR__ . "/MailPanel.latte");
+		$template->registerFilter(new Nette\Latte\Engine);
 		$template->data = array();
-		if(isSet(self::$container->queue) && self::$container->queue instanceof ArrayList) {
-			$template->data = self::$container->queue;
+		if($section->offsetExists("queue") && $section->offsetGet("queue") instanceof Nette\ArrayList) {
+			$template->data = $section->queue;
+			if(!$section->offsetExists("count")) $section->count = self::DEFAULT_COUNT;
+			$template->count = $section->count;
 		}
 		$template->render();
 		return ob_get_clean();
 	}
-
-	/**
-	 * Register this panel
-	 *
-	 * @param Context of this application
-	 */
-	public static function register(IDiContainer $context)
-	{
-		if (self::$registered) {
-			throw new InvalidStateException("Mail panel is already registered");
-		}
-		// Register panel
-		Debugger::addPanel(new self);
-		// Switch mailers
-		$context->removeService("mailer");
-		$context->addService("mailer", new SessionDummyMailer());
-		// Set routes to service presenter
-		$router = $context->getService("router");
-		$router[] = new Route("mail-panel/delete-all", array(
-			"presenter"		=> "MailPanel",
-			"action"		=> "deleteAll"
-		));
-		$router[] = new Route("mail-panel/delete/<id>", array(
-			"presenter"		=> "MailPanel",
-			"action"		=> "delete"
-		));
-		$router[] = new Route("mail-panel/show-more/<count>", array(
-			"presenter"		=> "MailPanel",
-			"action"		=> "showMore"
-		));
-		$router[] = new Route("mail-panel/show-less/<count>", array(
-			"presenter"		=> "MailPanel",
-			"action"		=> "showLess"
-		));
-		$router[] = new Route("mail-panel/detail/<id>", array(
-			"presenter"		=> "MailPanel",
-			"action"		=> "detail"
-		));
-		self::$registered = TRUE;
+	
+	private function getStorage() {
+		return self::$session->getSection(SessionDumpMailer::SECTION_NAME);
 	}
 	
-	/**
-	 * Return mail with mailer set in context. (It should be SessionDummyMailer)
-	 * @return Mail 
-	 */
-	public static function getMailPrototype() {
-		$ret = new Mail();
-		$ret->setMailer(Environment::getMailer());
-		return $ret;
+	private function returnWay() {
+		header("Location: ".self::$request->getReferer());
+		exit;
+	}
+	
+	private function handleDeleteAll() {
+		unset($this->getStorage()->queue);
+		$this->returnWay();
+	}
+	private function handleDelete($id) {
+		$storage = $this->getStorage();
+		foreach($storage->queue as $key => $row) {
+			if($key == $id) $storage->queue->offsetUnset($key);
+		}
+		$this->returnWay();
+	}
+	
+	private function handleShowMore($count) {
+		if (!is_numeric($count) || $count <= 0) {
+			return;
+		}
+		$storage = $this->getStorage();
+		if(!$storage->offsetExists("count")) $storage->count = self::DEFAULT_COUNT;
+		$storage->count = $storage->count+$count;
+		$this->returnWay();
+	}
+	
+	private function handleShowLess($count) {
+		if (!is_numeric($count) || $count <= 0) {
+			return;
+		}
+		$storage = $this->getStorage();
+		if(!$storage->offsetExists("count")) $storage->count = MailPanel::DEFAULT_COUNT;
+		if($storage->count-$count < 0) {
+			$this->returnWay();
+		}
+		$storage->count = $storage->count-$count;
+		$this->returnWay();
 	}
 }
