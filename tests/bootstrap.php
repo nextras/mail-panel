85d4d7f217d320f7066564bbdd14822a86119d7a
Jan Tvrdik <jan.tvrdik@gmail.com>
2019-05-12 17:52:32 +0200

    tests: change temp path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,6 @@ require __DIR__ . '/../vendor/autoload.php';
 
 Tester\Environment::setup();
 
-define('TEMP_DIR', __DIR__ . '/tmp/' . getmypid());
+define('TEMP_DIR', __DIR__ . '/../temp/' . getmypid());
 @mkdir(dirname(TEMP_DIR)); // @ - directory may already exist
 Tester\Helpers::purge(TEMP_DIR);
